f941a10c0515ee4487d541a297d6e5fa80a4ba90
Yume <poppym0232@gmail.com>
2023-06-08 17:40:37 +0900

    create tab screen

---
+++ app/src/main/java/com/example/takada_kenshi_no_densetsu_android/ui/densetsu/DensetsuScreen.kt
@@ -0,0 +1,7 @@
+package com.example.takada_kenshi_no_densetsu_android.ui.densetsu
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DensetsuScreen() {
+}
---
+++ app/src/main/java/com/example/takada_kenshi_no_densetsu_android/ui/densetsuList/DensetsuListScreen.kt
@@ -0,0 +1,7 @@
+package com.example.takada_kenshi_no_densetsu_android.ui.densetsuList
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DensetsuListScreen() {
+}
