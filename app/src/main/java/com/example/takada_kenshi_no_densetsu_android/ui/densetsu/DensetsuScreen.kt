ab5df99a58c74b3efd2c4ddb0124db219b68884d
Yume <poppym0232@gmail.com>
2023-06-08 17:43:29 +0900

    add screen's text

--- app/src/main/java/com/example/takada_kenshi_no_densetsu_android/ui/densetsu/DensetsuScreen.kt
+++ app/src/main/java/com/example/takada_kenshi_no_densetsu_android/ui/densetsu/DensetsuScreen.kt
@@ -1,7 +1,9 @@
 package com.example.takada_kenshi_no_densetsu_android.ui.densetsu
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun DensetsuScreen() {
+    Text("DensetsuScreen")
 }
