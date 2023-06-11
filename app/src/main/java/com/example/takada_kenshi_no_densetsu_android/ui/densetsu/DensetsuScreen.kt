b7179963bb444a3607eb0ba02f977d4b287af63d
Yume <poppym0232@gmail.com>
2023-06-12 01:26:20 +0900

    add success state

--- app/src/main/java/com/example/takada_kenshi_no_densetsu_android/ui/densetsu/DensetsuScreen.kt
+++ app/src/main/java/com/example/takada_kenshi_no_densetsu_android/ui/densetsu/DensetsuScreen.kt
@@ -1,8 +1,14 @@
 package com.example.takada_kenshi_no_densetsu_android.ui.densetsu
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -10,9 +16,43 @@ fun DensetsuScreen(
     densetsuViewModel: DensetsuViewModel = hiltViewModel()
 ) {
 
-    LaunchedEffect(Unit){
-        densetsuViewModel.getDensetsu()
-    }
+    val densetsuState = densetsuViewModel.densetsuState.collectAsState()
 
-    Text("DensetsuScreen")
+}
+
+@Composable
+fun DensetsuContent(
+    densetsuState: DensetsuState,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            when (densetsuState) {
+                is DensetsuState.Success -> {
+                    SuccessView(
+                        densetsuState.densetsu.text
+                    )
+                }
+
+                else -> {
+
+                }
+            }
+            Button(onClick = onClick) {
+                Text(text = "伝説を探す")
+            }
+        }
+    }
+}
+
+@Composable
+fun SuccessView(
+    text: String
+){
+    Text(text = text)
 }
